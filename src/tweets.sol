8e1e4a272e5c142a1e11c3860c6b1e003d945c08
Suyog-Dhakal <suyogdhakal8@gmail.com>
2022-07-09 20:47:32 +0545

    smart contract

--- src/tweets.sol
+++ src/tweets.sol
@@ -1,16 +1,15 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.7;
 
 contract tweets {
-
     address public owner;
     uint256 private counter;
 
     constructor() {
         counter = 0;
         owner = msg.sender;
-     }
+    }
 
     struct tweet {
         address tweeter;
@@ -19,7 +18,7 @@ contract tweets {
         string tweetImg;
     }
 
-    event tweetCreated (
+    event tweetCreated(
         address tweeter,
         uint256 id,
         string tweetTxt,
@@ -28,30 +27,43 @@ contract tweets {
 
     mapping(uint256 => tweet) Tweets;
 
-    function addTweet(
-        string memory tweetTxt,
-        string memory tweetImg
-        ) public payable {
-            require(msg.value == (1 ether), "Please submit 1 Matic");
-            tweet storage newTweet = Tweets[counter];
-            newTweet.tweetTxt = tweetTxt;
-            newTweet.tweetImg = tweetImg;
-            newTweet.tweeter = msg.sender;
-            newTweet.id = counter;
-            emit tweetCreated(
-                msg.sender, 
-                counter, 
-                tweetTxt, 
-                tweetImg
-            );
-            counter++;
+    function addTweet(string memory tweetTxt, string memory tweetImg)
+        public
+        payable
+    {
+        require(msg.value == (0.1 ether), "Submit 1 rinkeby eth");
+        /*"newtweet" is the  instant of struct "tweet" 
+        and storing in "Tweets" mapping with counter position */
+        tweet storage newTweet = Tweets[counter];
 
-            payable(owner).transfer(msg.value);
+        newTweet.tweetTxt = tweetTxt;
+        newTweet.tweetImg = tweetImg;
+        newTweet.tweeter = msg.sender;
+        newTweet.id = counter;
+
+        emit tweetCreated(msg.sender, counter, tweetTxt, tweetImg);
+
+        counter++;
+
+        //transfering ether to the owner
+        payable(owner).transfer(msg.value);
     }
 
-    function getTweet(uint256 id) public view returns (string memory, string memory, address){
-        require(id < counter, "No such Tweet");
+    function getTweet(uint256 id)
+        public
+        view
+        returns (
+            string memory,
+            string memory,
+            address
+        )
+    {
+        /*the two parameters string are 
+        tweetImg source, 
+        tweetTxt and 
+        the address of the person who made the tweet */
 
+        require(id < counter, "No such Tweet");
         tweet storage t = Tweets[id];
         return (t.tweetTxt, t.tweetImg, t.tweeter);
     }
